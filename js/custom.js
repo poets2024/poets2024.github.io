743463832e6e0b25ffdd5139668d200165c0bd65
poets2024 <162753504+poets2024@users.noreply.github.com>
2024-04-30 22:39:48 -0700

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -6,7 +6,7 @@
 (function($){
 	$(document).ready(function(){
 	
-		$(".banner-image").backstretch('images/banner-0.jpg');
+		$(".banner-image").backstretch('images/banner-0.png');
 		
 		// Fixed header
 		//-----------------------------------------------
